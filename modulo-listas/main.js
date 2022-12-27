5c9200e939d2110b6a1901575ffaad70fcfda7e5
Norman Leyva <nleyvagarcia@gmail.com>
2022-12-27 23:08:14 +0100

    update ejercicio listas

---
+++ modulo-listas/main.js
@@ -0,0 +1,7 @@
+let miNombre = "Norman";
+let miEdad = 38;
+let isDeveloper = true;
+const bday = new Date(1984, 3, 24);
+let favBook = "Davinci Code";
+
+let miLista = [miNombre, miEdad, isDeveloper, bday, favBook];
